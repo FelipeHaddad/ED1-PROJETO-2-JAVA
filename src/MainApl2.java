b2bde5c22846c75909bf006967e7293753aa09ee
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-05-23 12:38:06 -0300

    Update MainApl2.java

--- src/MainApl2.java
+++ src/MainApl2.java
@@ -25,7 +25,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class MainApl2 {
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		LinkedListOriginal list = new LinkedListOriginal();
 		DLinkedList novaLista = new DLinkedList();
@@ -58,21 +58,21 @@ public class MainApl2 {
 			novaLista.append(String.valueOf(id), nome, nota);
 		}
 		scanner.close();
-		
+
 		System.out.println(">>>>>>>>>> Dados originais (sistema legado) >>>>>>>>>>");
 		System.out.println(list);
 		System.out.println("<<<<<<<<<< Dados originais (sistema legado) <<<<<<<<<<\n");
-		
+
 		DLinkedList fixedList = Operation.map(list);
 		System.out.println(">>>>>>>>>> Dados convertidos para a nova representação dos dados >>>>>>>>>>");
 		System.out.println(fixedList);
 		System.out.println("<<<<<<<<<< Dados convertidos para a nova representação dos dados <<<<<<<<<<\n");
-		
+
 		DLinkedList filteredGradedList = Operation.filterRemoveNonGraded(fixedList);
 		System.out.println(">>>>>>>>>> Lista filtrada (somente notas válidas) >>>>>>>>>>");
 		System.out.println(filteredGradedList);
 		System.out.println("<<<<<<<<<< Lista filtrada (somente notas válidas) <<<<<<<<<<\n");
-		
+
 		DLinkedList filteredNonGradedList = Operation.filterRemoveGraded(fixedList);
 		System.out.println(">>>>>>>>>> Lista filtrada (somente 'ausência de nota') >>>>>>>>>>");
 		System.out.println(filteredNonGradedList);
@@ -82,43 +82,44 @@ public class MainApl2 {
 		System.out.println(">>>>>>>>>> Média das notas válidas >>>>>>>>>>");
 		System.out.println(average);
 		System.out.println("<<<<<<<<<< Média das notas válidas <<<<<<<<<<\n");
-		
+
 		DLinkedList aboveAverageList = Operation.filterRemoveBelowAverage(filteredGradedList, average);
 		System.out.println(">>>>>>>>>> Lista com notas acima da média >>>>>>>>>>");
 		System.out.println(aboveAverageList);
 		System.out.println("<<<<<<<<<< Lista com notas acima da média <<<<<<<<<<\n");
-		
-		StringBuilder contents = new StringBuilder();
+
+		String contents = Operation.mapToString(fixedList);
 		System.out.println(">>>>>>>>>> Lista mapeada para uma única string >>>>>>>>>>");
 		System.out.println(contents);
 		System.out.println("<<<<<<<<<< Lista mapeada para uma única string <<<<<<<<<<\n");
-		Node aux = aboveAverageList.getHead(); // ou o jeito que pega o primeiro nó
+
+
+		Node aux = novaLista.getHead(); // ou o jeito que pega o primeiro nó
+		String conteudo = "";
 		while (aux != null) {
 			// Aqui pegue o ID completo — tipo String "23.S1-111"
-			String idCompleto = aux.getId(); // tem que ser uma String com o valor completo!
+			String idCompleto = ("23.S1-" + aux.getId()); // tem que ser uma String com o valor
+			// completo!
 			String nome = aux.getNome();
 			double nota = aux.getNota();
 
-			contents.append(idCompleto)
-					.append(";")
-					.append(nome)
-					.append(";")
-					.append(nota)
-					.append("\n");
-
+			conteudo = conteudo + idCompleto
+					 + (";")
+					 + (nome)
+					 + (";")
+					 + (nota)
+					 + ("\n");
 			aux = aux.getProximo();
 		}
 
-		String conteudoFinal = contents.toString();
-
 		// Depois salva no arquivo
 		try (FileWriter fw = new FileWriter("dados.csv")) {
-			fw.write(conteudoFinal);
+			fw.write(conteudo);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		
+
 		Node test1 = fixedList.getNode("23.S1-999");
 		System.out.println(">>>>>>>>>> test1 >>>>>>>>>>\n" + test1 + "\n<<<<<<<<<< test1 <<<<<<<<<<\n");
 
@@ -133,14 +134,16 @@ public class MainApl2 {
 
 		DLinkedList testList = new DLinkedList();
 		testList.insert("ABC", "John Doe", 4.7f);
-		testList.insert("XYZ", "Jane Doe", 9.9f);
+		testList.append("XYZ", "Jane Doe", 9.9f);
 		testList.insert("321", "Test", 2.3f);
-		testList.insert("Nothing", "Yada yada yada", 99.9f);
+		testList.append("Nothing", "Yada yada yada", 99.9f);
 		System.out.println(">>>>>>>>>> testList >>>>>>>>>>\n" + testList  + "\n<<<<<<<<<< testList <<<<<<<<<<\n");
+
 		System.out.println("testList.getHead(): " + testList.getHead());
 		System.out.println("testList.getTail(): " + testList.getTail());
 		System.out.println("testList.removeHead(): " + testList.removeHead());
 		System.out.println("testList.removeTail(): " + testList.removeTail() + '\n');
+
 		System.out.println(">>>>>>>>>> testList >>>>>>>>>>\n" + testList  + "\n<<<<<<<<<< testList <<<<<<<<<<\n");
 		System.out.println("testList.getHead(): " + testList.getHead());
 		System.out.println("testList.getTail(): " + testList.getTail());
@@ -149,9 +152,9 @@ public class MainApl2 {
 		System.out.println("testList.getHead(): " + testList.getHead());
 		System.out.println("testList.getTail(): " + testList.getTail() + '\n');
 		testList.insert("qwerty", "QWERTY", 1.2f);
-		testList.insert("WASD", "wasd", 3.4f);
+		testList.append("WASD", "wasd", 3.4f);
 		testList.insert("ijkl", "IJKL", 5.6f);
-		testList.insert("1234", "Um Dois Tres Quatro", 7.8f);
+		testList.append("1234", "Um Dois Tres Quatro", 7.8f);
 		System.out.println(">>>>>>>>>> testList >>>>>>>>>>\n" + testList  + "\n<<<<<<<<<< testList <<<<<<<<<<\n");
 		testList.clear();
 		System.out.println(">>>>>>>>>> testList.clear() >>>>>>>>>>\n" + testList  + "\n<<<<<<<<<< testList.clear() <<<<<<<<<<\n");
